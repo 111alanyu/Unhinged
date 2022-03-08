0e3918b06233ee5d255610af6925cacb4597698d
Alan Yu <44562288+111alanyu@users.noreply.github.com>
2022-03-08 03:17:08 -0800

    Initial commit of attribute translator

---
+++ Unhinged/AttributeTranslator.cpp
@@ -0,0 +1,8 @@
+//
+//  AttributeTranslator.cpp
+//  Unhinged
+//
+//  Created by Alan Yu on 3/8/22.
+//
+
+#include <stdio.h>
---
+++ Unhinged/AttributeTranslator.h
@@ -0,0 +1,12 @@
+//
+//  AttributeTranslator.h
+//  Unhinged
+//
+//  Created by Alan Yu on 3/8/22.
+//
+
+#ifndef AttributeTranslator_h
+#define AttributeTranslator_h
+
+
+#endif /* AttributeTranslator_h */
